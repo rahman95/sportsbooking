83bad040a6dce75290fc6a160a46a1ee0d4503a5
Rahman Younus <rahman_95@live.co.uk>
2016-04-09 03:00:52 +0100

    Added PDF feature for Facility Bookings

---
+++ resources/views/pdffacility.blade.php
@@ -0,0 +1,10 @@
+
+<img src="img/pdf/header.png" class="center-block" style="width: 100%; height: 100%">
+    <div style= "text-align: center;">
+    <h5>Booking ID: {{ $facility->id }}</h5>
+    <h5>Facility Type: {{ $facility->facilitytype }}</h5>
+    <h5>Booking Date: {{ $facility->bookingdate }}</h5>
+    <h5>Booking Time: {{ $facility->bookingtime }}:00</h5>
+    <h5>Member ID: {{ $facility->bookedby }}</h5>
+    </div>
+<img src="img/pdf/footer.png" class="center-block" style="width: 100%; height: 100%">
